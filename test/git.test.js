5c7ef33ffc88836d8f6581ecab6472f309b53ea4
BlaiseCosico <BlaiseCosico@gmail.com>
2021-06-03 15:44:11 +0800

    second pr test

--- test/git.test.js
+++ test/git.test.js
@@ -4,6 +4,7 @@ const GitCommand = require("../models/git-command");
 const chai = require('chai');
 const expect = chai.expect;
 //adding git change
+//asda
 describe("Testing add()", function(){
 
     it('Should success with exact path_file "views/index.html"', function(){
